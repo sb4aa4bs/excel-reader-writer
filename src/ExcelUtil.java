aa2edeff8bcb155de3565ec17bec7ec3988c0727
bharathkumar.kandaswamy1@t-mobile.com <bharathkumar.kandaswamy1@t-mobile.com>
2017-04-13 13:52:58 -0700

    Signed-off-by: bharathkumar.kandaswamy1@t-mobile.com <bharathkumar.kandaswamy1@t-mobile.com>

--- src/ExcelUtil.java
+++ src/ExcelUtil.java
@@ -1,9 +1,23 @@
+import java.util.HashMap;
+import java.util.Map;
 
 public class ExcelUtil {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+	}
+
+	public static Map<String, String> readFromTab(String xlFileName, String tabName) {
+		Map<String, String> xlDataMap = new HashMap<String, String>();
+
+		return xlDataMap;
+	}
+
+	public static boolean writeIntoTab(Map<String, String> xlDataMap) {
+		boolean writeStatus = false;
+
+		return writeStatus;
 	}
 
 }
