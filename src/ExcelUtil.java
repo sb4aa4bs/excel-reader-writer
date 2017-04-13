1c4314998e8c3bf55000c3de14427f5af1da53fe
bharathkumar.kandaswamy1@t-mobile.com <bharathkumar.kandaswamy1@t-mobile.com>
2017-04-13 14:29:49 -0700

    Signed-off-by: bharathkumar.kandaswamy1@t-mobile.com <bharathkumar.kandaswamy1@t-mobile.com>

--- src/ExcelUtil.java
+++ src/ExcelUtil.java
@@ -1,21 +1,112 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class ExcelUtil {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		ExcelUtil.writeIntoTab(new HashMap<String, String>());
+		ExcelUtil.readFromTab("", "");
 	}
 
 	public static Map<String, String> readFromTab(String xlFileName, String tabName) {
 		Map<String, String> xlDataMap = new HashMap<String, String>();
+		try {
+			FileInputStream file = new FileInputStream(new File("C:\\TEMP\\poi_demo.xlsx"));
+
+			// Create Workbook instance holding reference to .xlsx file
+			XSSFWorkbook workbook = new XSSFWorkbook(file);
+
+			// Get first/desired sheet from the workbook
+			XSSFSheet sheet = workbook.getSheetAt(0);
+
+			// Iterate through each rows one by one
+			Iterator<Row> rowIterator = sheet.iterator();
+			while (rowIterator.hasNext()) {
+				Row row = rowIterator.next();
+				// For each row, iterate through all the columns
+				Iterator<Cell> cellIterator = row.cellIterator();
+
+				while (cellIterator.hasNext()) {
+					Cell cell = cellIterator.next();
+					// Check the cell type and format accordingly
+					switch (cell.getCellType()) {
+					case Cell.CELL_TYPE_NUMERIC:
+						System.out.print(cell.getNumericCellValue() + "\t");
+						break;
+					case Cell.CELL_TYPE_STRING:
+						System.out.print(cell.getStringCellValue() + "\t");
+						break;
+					}
+				}
+				System.out.println("");
+			}
+			file.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
 		return xlDataMap;
 	}
 
 	public static boolean writeIntoTab(Map<String, String> xlDataMap) {
 		boolean writeStatus = false;
+
+		// Blank workbook
+		XSSFWorkbook workbook = new XSSFWorkbook();
+
+		// Create a blank sheet
+		XSSFSheet sheet = workbook.createSheet("Employee Data");
+
+		// This data needs to be written (Object[])
+		Map<String, Object[]> data = new TreeMap<String, Object[]>();
+		data.put("1", new Object[] { "ID", "NAME", "LASTNAME" });
+		data.put("2", new Object[] { 1, "Amit", "Shukla" });
+		data.put("3", new Object[] { 2, "Lokesh", "Gupta" });
+		data.put("4", new Object[] { 3, "John", "Adwards" });
+		data.put("5", new Object[] { 4, "Brian", "Schultz" });
+
+		// Iterate over data and write to sheet
+		Set<String> keyset = data.keySet();
+
+		int rownum = 0;
+		for (String key : keyset) {
+			// create a row of excelsheet
+			Row row = sheet.createRow(rownum++);
+
+			// get object array of prerticuler key
+			Object[] objArr = data.get(key);
+
+			int cellnum = 0;
+
+			for (Object obj : objArr) {
+				Cell cell = row.createCell(cellnum++);
+				if (obj instanceof String) {
+					cell.setCellValue((String) obj);
+				} else if (obj instanceof Integer) {
+					cell.setCellValue((Integer) obj);
+				}
+			}
+		}
+		try {
+			// Write the workbook in file system
+			FileOutputStream out = new FileOutputStream(new File("C:\\TEMP\\poi_demo.xlsx"));
+			workbook.write(out);
+			out.close();
+			System.out.println("howtodoinjava_demo.xlsx written successfully on disk.");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
 		return writeStatus;
 	}
